a0e1555af05a0eb0e3a58676b564fa03c512c5e2
Lorraine <k6xu@ucsd.edu>
2021-03-29 02:46:05 -0700

    sponsors and contact sections done

--- src/components/section/Contact.js
+++ src/components/section/Contact.js
@@ -1,13 +1,50 @@
 import React from 'react';
 import './Contact.css';
-
+import { Button } from './Button';
 export default function Contact(){
     return (
       <section id="contact">
         <div className="contact-container">
             <h1>Contact</h1>
-            <p>input your name</p>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <div className='input-areas'>
+              <h2>Name:</h2>
+              <form>
+                <input
+                  className='footer-input'
+                  name='name'
+                  type='name'
+                  placeholder='Your Name'
+                />
+
+              </form>
+              <h2>Eamil:</h2>
+              <form>
+                <input
+                  className='footer-input'
+                  name='email'
+                  type='email'
+                  placeholder='Your Email'
+                />
+
+              </form>
+              <h2>Message:</h2>
+              <form>
+                <input
+                  className='footer-input'
+                  name='message'
+                  type='message'
+                  placeholder='Your Message'
+                />
+
+              </form>
+              <Button buttonStyle='btn--outline'>Submit!</Button>
+            </div>
         </div>
+
       </section>
     );
 }
--- src/components/section/Sponsors.js
+++ src/components/section/Sponsors.js
@@ -5,16 +5,20 @@ export default function Sponsors(){
         <section id="sponsors">
             <div className="sponsor-container">
               <h1>Sponsors</h1>
-              <h1><img name="cseslogo" src="/sponsors/yelp.png"
-                       width = "280" height = "80" alt="logo"></img></h1>
-              <h1><img name="cseslogo" src="/sponsors/cse-ucsd.png"
-                       width = "280" height = "80" alt="logo"></img></h1>
-              <h1><img name="cseslogo" src="/sponsors/viasat.png"
-                       width = "280" height = "80" alt="logo"></img></h1>
-              <h1><img name="cseslogo" src="/sponsors/riot-logo-black-text.png"
-                       width = "300" height = "200" alt="logo"></img></h1>
-              <h1><img name="cseslogo" src="/sponsors/lab_society.png"
-                       width = "300" height = "200" alt="logo"></img></h1>
+              <div className="img1">
+              <img name="cseslogo" src="/sponsors/yelp.png"
+                       width = "280" height = "80" alt="logo"></img>
+              <img name="cseslogo" src="/sponsors/cse-ucsd.png"
+                       width = "280" height = "80" alt="logo"></img>
+
+              <img name="cseslogo" src="/sponsors/viasat.png"
+                       width = "280" height = "80" alt="logo"></img>
+              <img name="cseslogo" src="/sponsors/riot-logo-black-text.png"
+                       width = "300" height = "200" alt="logo"></img>
+
+              <img name="cseslogo" src="/sponsors/lab_society.png"
+                       width = "300" height = "200" alt="logo"></img>
+              </div>
             </div>
         </section>
     );
